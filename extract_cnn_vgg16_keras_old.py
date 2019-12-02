8ac8131121fa2bfc86063bd20c862923c5bf92c3
raojay7 <6130116160@email.ncu.edu.cn>
2019-12-02 17:59:54 +0800

    备份

---
+++ extract_cnn_vgg16_keras_old.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# Author: yongyuan.name
+import os
+# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+import numpy as np
+from numpy import linalg as LA
+
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input
+from keras.models import Model
+
+class VGGNet:
+    def __init__(self):
+        # weights: 'imagenet'
+        # pooling: 'max' or 'avg'
+        # input_shape: (width, height, 3), width and height should >= 48
+        self.input_shape = (224, 224, 3)
+        self.weight = 'imagenet'
+        self.pooling = 'max'
+
+        #include_top = False表示只需要卷积层，但是我这里需要测试全连接所以include为true
+        self.model = VGG16(weights = self.weight, input_shape = (self.input_shape[0], self.input_shape[1], self.input_shape[2]), pooling = self.pooling, include_top = False)
+        # self.basemodel = VGG16(weights=self.weight,)
+        #特征抽取可以改变全连接层，暂时没用
+        # self.model = Model(input=self.basemodel.input,
+        #                    outputs=self.basemodel.get_layer('fc1').output)
+        self.model.predict(np.zeros((1, 224, 224, 3)))
+
+    '''
+    Use vgg16 model to extract features
+    Output normalized feature vector
+    '''
+    def extract_feat(self, img_path):
+        img = image.load_img(img_path, target_size=(self.input_shape[0], self.input_shape[1]))
+        img = image.img_to_array(img)
+        img = np.expand_dims(img, axis=0)
+        img = preprocess_input(img)
+        feat = self.model.predict(img)
+        # print("------"+str(feat.shape[0])+"-----"+str(feat.shape[1]))
+        norm_feat = feat[0]/LA.norm(feat[0])
+        return norm_feat
